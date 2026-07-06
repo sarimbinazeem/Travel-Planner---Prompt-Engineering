f8987f998ec15c2e5aa5c503cee0fc36b75e6767
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-06 12:30:19 +0500

    Phase 2: Created Pages

---
+++ travel planner/src/pages/DestinationDetails.jsx
@@ -0,0 +1,9 @@
+function DestinationDetails() {
+  return (
+    <h1 className="text-3xl font-bold text-center mt-10">
+      Destination Details Page
+    </h1>
+  );
+}
+
+export default DestinationDetails;
---
+++ travel planner/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <h1 className="text-3xl font-bold text-center mt-10">
+      404 - Page Not Found
+    </h1>
+  );
+}
+
+export default NotFound;
